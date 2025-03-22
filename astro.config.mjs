44b4aa6038b0f964c25939aedb23da07ddccab31
Cristian Balza <devcrisnk23@gmail.com>
2025-03-22 12:36:18 -0400

    3commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  base: '/PorfolioWeb' // Reemplaza 'nombre-del-repositorio' con el nombre de tu repositorio en GitHub
 });
